4daf20e32cdf3a80fe3448d2bbaf659ca26f656d
skhhks3953 <hyhow7726@gmail.com>
2018-04-05 01:02:42 +0800

    solving merge conflict

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -3,11 +3,7 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 	int option;
-=======
-	int decision;
->>>>>>> c54d8d3c805f9f02cf4bc105967f529ddf1184c7
 	
 	cout << "1.Food  2.Drinks" << endl;
 	cout << "Please enter an option : ";
